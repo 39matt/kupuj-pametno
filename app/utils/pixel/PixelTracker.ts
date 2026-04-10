92b0117e66bebfd4824ca3ab56d496eb5076c437
39matt <matija.todosijevic.i4@gmail.com>
2026-04-10 19:56:51 +0200

    edit: meta pixel new pixel id

--- app/utils/pixel/PixelTracker.ts
+++ app/utils/pixel/PixelTracker.ts
@@ -5,7 +5,7 @@ import ReactPixel from "react-facebook-pixel";
 
 const PixelTracker = () => {
     useEffect(() => {
-        const pixelId = "1222071759283044";
+        const pixelId = "1702110450341381";
         ReactPixel.init(pixelId);
         ReactPixel.pageView();
     }, []);
